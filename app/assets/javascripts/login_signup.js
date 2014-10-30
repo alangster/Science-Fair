04d2c79e9f5208736f349382b707b13948a61c67
Alex Lang & Perry Anderson <pair+alangster.peryanderson@devbootcamp.com>
2014-10-30 17:31:23 -0500

    sign up and login toggle and switch display

--- app/assets/javascripts/login_signup.js
+++ app/assets/javascripts/login_signup.js
@@ -1,12 +1,18 @@
 $(function() {
   $('.login-link').on('click', function(e) {
     e.preventDefault();
+    if ($('.sign-up-form').css('display') !== 'none') {
+      $('.sign-up-form').toggle();
+    };
     $('.login-form').toggle();
     // ajax
   });
 
   $('.sign-up-link').on('click', function(e){
     e.preventDefault();
+    if ($('.login-form').css('display') !== 'none') {
+      $('.login-form').toggle();
+    };
     $('.sign-up-form').toggle();
     // ajax
   });
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,16 @@
 class SessionsController < ApplicationController
 
+  def create
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.password == params[:user][:password]
+      session[:user_id] = @user.id
+    else
+      @error = "That wasn't quite right"
+    end
+  end
+
+  def destroy
+
+  end
+
 end
