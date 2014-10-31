63b5d736e65d572b06fb325b6c185b3de3f762fc
Alex Lang & Perry Anderson <pair+alangster.peryanderson@devbootcamp.com>
2014-10-31 14:43:56 -0500

    some css and basic form laid down

--- app/controllers/posters_controller.rb
+++ app/controllers/posters_controller.rb
@@ -9,4 +9,18 @@ class PostersController < ApplicationController
     @comments = @poster.comments
   end
 
+  def new
+
+    @poster = Poster.new
+  end
+
+  def create
+    @poster = Poster.new(params[:poster])
+    @poster.creator = current_user
+      if @poster.save
+        redirect_to @poster
+      else
+        @error = "The poster was not created"
+      end
+  end
 end
