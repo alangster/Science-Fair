059b01ef5031e66d9ae754f5cc50a8cb66327449
Vince <vlmist315@gmail.com>
2014-11-01 18:58:55 -0500

    fixed issue with the user profile page

--- app/assets/javascripts/multiple_poster.js
+++ app/assets/javascripts/multiple_poster.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
 
-  var posters = $(".poster");
-  var poster_authors = $(".poster-authors");
-  var poster_tags = $(".poster-tags");
+  // var posters = $(".poster");
+  // var poster_authors = $(".poster-authors");
+  // var poster_tags = $(".poster-tags");
 
-  poster_authors.hide();
-  poster_tags.hide();
+  // poster_authors.hide();
+  // poster_tags.hide();
 
-  posters.mouseover(function(event){
-    $(this).find(".poster-authors").show();
-    $(this).find(".poster-tags").show(); 
-  });
-  posters.mouseout(function(event){
-    $(this).find(".poster-authors").hide();
-    $(this).find(".poster-tags").hide(); 
-  });
+  // posters.mouseover(function(event){
+  //   $(this).find(".poster-authors").show();
+  //   $(this).find(".poster-tags").show(); 
+  // });
+  // posters.mouseout(function(event){
+  //   $(this).find(".poster-authors").hide();
+  //   $(this).find(".poster-tags").hide(); 
+  // });
 })
